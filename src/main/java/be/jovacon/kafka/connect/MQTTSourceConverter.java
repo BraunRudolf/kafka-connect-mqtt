bd9dd116d439059b4911e259dbe73c90d61b16bd
zhuchunlai <zhuchunlai@wl-times.com>
2024-07-04 22:14:19 +0800

    Support bytes messages.

--- src/main/java/be/jovacon/kafka/connect/MQTTSourceConverter.java
+++ src/main/java/be/jovacon/kafka/connect/MQTTSourceConverter.java
@@ -4,6 +4,7 @@ import be.jovacon.kafka.connect.config.MQTTSourceConnectorConfig;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.header.ConnectHeaders;
 import org.apache.kafka.connect.source.SourceRecord;
+import org.apache.kafka.connect.storage.Converter;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,13 +15,13 @@ import java.util.HashMap;
  * Converts a MQTT message to a Kafka message
  */
 public class MQTTSourceConverter {
+    private static final Logger log = LoggerFactory.getLogger(MQTTSourceConverter.class);
 
-    private MQTTSourceConnectorConfig mqttSourceConnectorConfig;
+    private static final HashMap<String, ?> EMPTY = new HashMap<>();
+    private final MQTTSourceConnectorConfig config;
 
-    private Logger log = LoggerFactory.getLogger(MQTTSourceConverter.class);
-
-    public MQTTSourceConverter(MQTTSourceConnectorConfig mqttSourceConnectorConfig) {
-        this.mqttSourceConnectorConfig = mqttSourceConnectorConfig;
+    public MQTTSourceConverter(MQTTSourceConnectorConfig config) {
+        this.config = config;
     }
 
     protected SourceRecord convert(String topic, MqttMessage mqttMessage) {
@@ -30,24 +31,19 @@ public class MQTTSourceConverter {
         headers.addInt("mqtt.message.id", mqttMessage.getId());
         headers.addInt("mqtt.message.qos", mqttMessage.getQos());
         headers.addBoolean("mqtt.message.duplicate", mqttMessage.isDuplicate());
+        headers.addString("mqtt.topic", topic);
 
-        // Kafka 1.0
-        /*SourceRecord sourceRecord = new SourceRecord(
-                new HashMap<>(),
-                new HashMap<>(),
-                this.mqttSourceConnectorConfig.getString(MQTTSourceConnectorConfig.KAFKA_TOPIC),
-                Schema.STRING_SCHEMA,
-                new String(mqttMessage.getPayload()));
-*/
         // Kafka 2.3
-        SourceRecord sourceRecord = new SourceRecord(new HashMap<>(),
-                new HashMap<>(),
-                this.mqttSourceConnectorConfig.getString(MQTTSourceConnectorConfig.KAFKA_TOPIC),
-                (Integer) null,
+        SourceRecord sourceRecord = new SourceRecord(
+                EMPTY,
+                EMPTY,
+                this.config.getString(MQTTSourceConnectorConfig.KAFKA_TOPIC),
+                null,
                 Schema.STRING_SCHEMA,
                 topic,
-                Schema.STRING_SCHEMA,
-                new String(mqttMessage.getPayload()),
+                // TODO Integration with Kafka Converter
+                Schema.BYTES_SCHEMA,
+                mqttMessage.getPayload(),
                 System.currentTimeMillis(),
                 headers);
         log.debug("Converted MQTT Message: " + sourceRecord);
